0c4fcad81c27839555e67dc90f18a38d109ae9f7
erfan gharib <erfangharib5@gmail.com>
2022-01-12 20:40:53 +0330

    add package-lock.json package.json and tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        'img': '40% auto',
+      },
+      gridRow: {
+        '1\/5': '1/5',
+      },
+      width: {
+        '46':'46.1%'
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
