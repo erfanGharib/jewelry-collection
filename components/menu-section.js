ad8a0ed72722b8acedb2101d69303c3f9588c2b8
erfan gharib <erfangharib5@gmail.com>
2022-01-19 21:45:39 +0330

    update product.js html code - update menu-section.js html code add eventlistener on them

--- components/menu-section.js
+++ components/menu-section.js
@@ -1,24 +1,37 @@
 import brands_$data, {products_$data} from '../js/main.js';
+import load_product_$func from './product.js';
 let sidebar_$dom = document.querySelector('#brands');
 
 export default function load_sidebar_sections_$func() {
     for (let data_num = 0; data_num < 5; data_num++) {
         brands_$data.then(data =>{
             sidebar_$dom.innerHTML += (`
-                <span class="sidebar-items w-full">
+                <span class="sidebar-items w-full relative">
                 <img 
-                    class="rounded-full row-1/5 w-16 h-16 mr-5 shadow-md" 
-                    src="../assets/images/${data[data_num].profile_img_src}" 
+                    class="rounded-full z-10 row-1/5 w-16 h-16 mr-5 shadow-md" 
+                    src="../assets/images/${data[data_num].name}/${data[data_num].profile_img_src}" 
                     alt="${data[data_num].name}"
                 >
-                <h3 class="text-lg font-semibold h-6 uppercase mt-2">${data[data_num].name}</h3>
-                <p class="product-length font-semibold text-opacity-40 text-black"></p>
+                <div class="flex flex-col">
+                    <h3 class="ml-3 text-lg font-semibold z-10 h-6 uppercase">${data[data_num].name}</h3>
+                    <p class="ml-3 product-length z-10 font-semibold text-opacity-40 text-black"></p>
+                </div>
+                <span style="transition: .4s;" class="w-0 h-full absolute z-0 bg-black bg-opacity-5"></span>
                 </span>
             `)
         })
         .then(()=>{
             products_$data.then(data=>{
                 document.querySelectorAll('.product-length')[data_num].innerHTML=`${data.length} products`
+            });
+
+            let sidebar_items_$dom = document.querySelectorAll('.sidebar-items');
+            sidebar_items_$dom[data_num].addEventListener('click', function() {
+                for (let index = 0; index < 5; index++) {
+                    sidebar_items_$dom[index].lastElementChild.classList.remove('sidebar-item-bg');
+                }
+                this.lastElementChild.classList.add('sidebar-item-bg');
+                load_product_$func(data_num);
             })
         })
     }
--- components/product.js
+++ components/product.js
@@ -1,38 +1,37 @@
 import {products_$data} from '../js/main.js';
 let product_sections_$dom = document.querySelector('#product-sections');
 
-export default function load_product_$func() {
-    for (let data_num = 0; data_num < 4; data_num++) {
-        products_$data.then(data =>{
+export default function load_product_$func(data_num) {
+    products_$data.then(data =>{
+        data[data_num].forEach((product_obj, product_index) => {
             product_sections_$dom.innerHTML += (`
                 <span class="w-full h-24 flex items-center justify-between border-b border-black border-opacity-10">
-                <h3 class="text-lg px-3 sm:px-5 font-bold">1</h3>
+                <h3 class="text-lg px-3 sm:px-5 font-bold">${data_num+=1}</h3>
                 <div class="flex items-center w-46 mr-auto">
                     <img 
                         class="border border-black border-opacity-10 w-12 h-12 sm:w-16 sm:h-16" 
-                        src="../assets/images/${data[data_num].img_src}" 
-                        alt="${data[data_num].name}"
+                        src="../assets/images/${data[data_num][product_index].base_url}${data[data_num][product_index].img_name}" 
+                        alt="${data[data_num][product_index].name}"
                     >
-                    <h3 class="font-bold capitalize sm:text-md text-sm sm:ml-10 ml-3">${data[data_num].name}</h3>
+                    <h3 class="font-bold capitalize sm:text-md text-sm sm:ml-10 ml-3">${data[data_num][product_index].name}</h3>
                 </div>
     
-                <p class="w-1/5 sm:w-1/4 pl-1 text-xs sm:text-sm mr-3">${data[data_num].material}</p>
+                <p class="w-1/5 sm:w-1/4 pl-1 text-xs sm:text-sm mr-3">${data[data_num][product_index].material}</p>
                 
                 <div class="flex w-1/4 justify-between items-center">
-                    <p class="text-xs sm:text-sm relative ">${data[data_num].price} &euro;</p>
+                    <p class="text-xs sm:text-sm relative ">${data[data_num][product_index].price} &euro;</p>
     
                     <div class="flex sm:flex-row items-center justify-center flex-col">
-                        <button class="ml-auto">
-                            <i class="ico-plus w-7 h-7 block rounded-full mx-2 bg-cover"></i>
-                            <button class="hidden">Explore</button>
-                            <button class="hidden">Buy</button>
+                        <button onclick="this.classList.toggle('plus-btn')" class="plus-btn rounded-full mx-2 h-7 flex">
+                            <span style="transition:.3s" class="opacity-100 h-7 py-1 px-2 text-sm bg-gray-200 text-gray-400 hover:text-gray-700">Buy</span>
+                            <span style="transition:.3s" class="opacity-100 h-7 py-1 px-2 text-sm bg-black hover:bg-opacity-70 text-white">Explore</span>
                         </button>
                         
-                        <i class="ico-heart block bg-cover w-7 h-7 sm:mt-0 mt-2 mr-0 lg:mr-0 sm:mr-3"></i>
+                        <button class="opacity-50 hover:opacity-100 ico-heart block bg-cover w-7 h-7 sm:mt-0 mt-2 mr-0 lg:mr-0 sm:mr-3"></button>
                     </div>
                 </div>
                 </span>
             `)
-        })
-    }
+        });
+    })
 }
