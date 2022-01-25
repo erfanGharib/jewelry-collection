e48802b91a2e39c74f929917c1cf0f5e74cbe58f
erfan gharib <erfangharib5@gmail.com>
2022-01-25 16:09:30 +0330

    call required functions - set event listeners on product and menu btn - almost done with project

--- js/main.js
+++ js/main.js
@@ -3,29 +3,33 @@ import product_menu_btn_$cls from './class.js';
 import load_sidebar_sections_$func from '../components/menu-section.js';
 // import load_product_$func from '../components/product.js';
 import load_banner_$func from '../components/banner.js';
+import load_product_$func from '../components/product.js';
 
 // load json data and convert to json
 const brands_$data = fetch('../data/brands.json').then(res => res.json());
 const products_$data = fetch('../data/products.json').then(res => res.json());
 
-document.body.onload=()=>{
-    load_sidebar_sections_$func();
-    load_banner_$func();
-}
+document.body.onload=async ()=>{
+    await load_product_$func();
+    await load_banner_$func();
+    await load_sidebar_sections_$func();
 
-// set sidebar and product button event listener
-document.querySelector('#products-btn').addEventListener('click', () => {
-    new product_menu_btn_$cls(
-        'product-preview',
-        '#products'
-    ).open_target_elem()
-});
-document.querySelector(".ico-menu").addEventListener('click', () => {
-    new product_menu_btn_$cls(
-        'translate-x-full',
-        '#sidebar'
-    ).open_target_elem()
-});
+    // set sidebar and product button event listener
+    await setTimeout(() => {
+        document.querySelector('#products-btn').addEventListener('click', ()=> {
+            new product_menu_btn_$cls(
+                'product-preview',
+                '#products'
+            ).open_target_elem()
+        });
+        document.querySelector(".ico-menu").addEventListener('click', ()=> {
+            new product_menu_btn_$cls(
+                'translate-x-full',
+                '#sidebar'
+            ).open_target_elem()
+        });
+    }, 2000);
+}
 
 // export json data
 export default brands_$data;
