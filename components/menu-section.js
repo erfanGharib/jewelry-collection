94d9753c727e852f291ab95537f74ff0d80aebae
erfan gharib <erfangharib5@gmail.com>
2022-01-25 16:06:32 +0330

    add change product move - change product by sidebar items onclick

--- components/menu-section.js
+++ components/menu-section.js
@@ -1,64 +1,76 @@
 import brands_$data, {products_$data} from '../js/main.js';
-import load_product_$func from './product.js';
 let sidebar_$dom = document.querySelector('#brands');
 let sidebar_item_index = 0;
 
-const banner_sliders_move_$obj = {
-    bannerBg: 1500,
-    profileImg: 128,
-    brandName: 288
-}
-const product_sliders_move_$obj = {
-    img: null,
-    name: null,
-    material: null,
-    price: null
-}
+const banner_sliders_move_$obj = [
+    {id: 'bannerBg', move: 1500,},
+    {id: 'profileImg', move: 128,},
+    {id: 'brandName', move: 288,}
+]
+const product_sliders_move_$obj = [
+    {id: 'prdctImg', move: 3,},
+    {id: 'prdctName', move: 24,},
+    {id: 'prdctMaterial', move: 20,},
+    {id: 'prdctPrice', move: 4,}
+]
 
 export default function load_sidebar_sections_$func() {
-    for (let data_num = 0; data_num < 5; data_num++) {
-        brands_$data.then(data =>{
+    brands_$data.then(data =>{
+        data.forEach((value, data_num) => {
+            // load all sidebar items
             sidebar_$dom.innerHTML += (`
                 <span class="sidebar-items w-full relative hover:bg-gray-600 hover:bg-opacity-5">
                 <img 
                     class="rounded-full z-10 row-1/5 w-16 h-16 mr-5 shadow-md" 
-                    src="../assets/images/${data[data_num].name}/${data[data_num].profile_img_src}" 
-                    alt="${data[data_num].name}"
+                    src="../assets/images/${value.name}/${value.profile_img_src}" 
+                    alt="${value.name}"
                 >
                 <div class="flex flex-col">
-                    <h3 class="ml-3 text-lg font-semibold z-10 h-6 uppercase">${data[data_num].name}</h3>
+                    <h3 class="ml-3 text-lg font-semibold z-10 h-6 uppercase">${value.name}</h3>
                     <p class="ml-3 product-length z-10 font-semibold text-opacity-40 text-black"></p>
                 </div>
                 <span style="transition: .4s;" class="w-0 h-full left-0 absolute z-0 bg-black bg-opacity-5"></span>
                 </span>
             `)
-        })
-        .then(()=>{
-            products_$data.then(data=>{
-                document.querySelectorAll('.product-length')[data_num].innerHTML=`${data.length} products`
-            });
 
+            // get each brand product length
+            products_$data.then(data=>{
+                document.querySelectorAll('.product-length')[data_num].innerHTML=(
+                    `${data[data_num].length} products`
+                )
+            });
+        })
+        
+        for (let index=0; index<data.length; index++) {
             let sidebar_items_$dom = document.querySelectorAll('.sidebar-items');
-            sidebar_items_$dom[data_num].addEventListener('click', function() {
+            sidebar_items_$dom[sidebar_item_index].lastElementChild.classList.add('sidebar-item-bg');
+
+            sidebar_items_$dom[index].addEventListener('click', function() {
+                // add / remove sidebar items background *********************
                 sidebar_items_$dom[sidebar_item_index].lastElementChild.classList.remove('sidebar-item-bg');
                 this.lastElementChild.classList.add('sidebar-item-bg');
-
-                // load_product_$func(data_num);
-
-                document.querySelector("#bannerBg").style.right=(
-                    `${banner_sliders_move_$obj.bannerBg * data_num}px`
-                )
-                document.querySelector("#profileImg").style.right=(
-                    `${banner_sliders_move_$obj.profileImg * data_num}px`
-                )
-                document.querySelector("#brandName").style.right=(
-                    `${banner_sliders_move_$obj.brandName * data_num}px`
-                )
+                
+                // change banner *********************************************
+                banner_sliders_move_$obj.forEach((value)=>{
+                    document.querySelector(`#${value.id}`).style.right=(
+                        `${value.move * index}px`
+                    )
+                })
                 document.querySelectorAll('.brand-info')[sidebar_item_index].classList.add('opacity-0');
-                document.querySelectorAll('.brand-info')[data_num].classList.remove('opacity-0');
+                document.querySelectorAll('.brand-info')[index].classList.remove('opacity-0');
 
-                sidebar_item_index=data_num;
+                // change product *********************************************
+                for (let index_ = 0; index_ < 3; index_++) {
+                    product_sliders_move_$obj.forEach((value)=>{
+                        document.querySelectorAll(`.${value.id}`)[index_].style.right=(
+                            `${value.move * index}rem`
+                        )
+                    })
+                }
+
+                // put last sidebar-item index in variable ********************
+                sidebar_item_index=index;
             })
-        })
-    }
+        }
+    })
 }
