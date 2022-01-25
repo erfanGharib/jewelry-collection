cfc822a1884a005a11cbb23e30e24b8e6076649d
erfan gharib <erfangharib5@gmail.com>
2022-01-25 16:02:30 +0330

    update banner.js

--- components/banner.js
+++ components/banner.js
@@ -15,25 +15,25 @@ export default function load_banner_$func() {
                 alt="${data[index].name}"
             >`;
 
-            brand_info+=`<p style="transition:1.5s;" class="brand-info absolute opacity-0 top-0 left-0">${data[index].info}</p>`
+            brand_info+=`<p class="brand-info transition-1/5 absolute opacity-0 top-0 left-0">${data[index].info}</p>`
         });
 
         banner_$dom.innerHTML+=(`
             <span class="absolute w-full h-full z-1- left-0 top-0 overflow-hidden">
-                <span style="transition:1.5s;" id="bannerBg" class="relative right-0 flex items-center h-full">
+                <span id="bannerBg" class="transition-1/5 relative right-0 flex items-center h-full">
                     ${bg_img_slider}
                 </span>
             </span>
 
             <div id="brand-profile" class="flex w-full sm:flex-row items-center sm:text-left text-center flex-col m-auto sm:m-0 sm:mt-auto relative">
                 <span class="relative right-0 min-w-32 mt-5 w-32 h-32 rounded-full shadow-md mb-10 block mr-7 overflow-hidden">
-                    <span style="transition:1.5s;" id="profileImg" class="flex items-start relative right-0">${profile_img_slider}</span>
+                    <span id="profileImg" class="transition-1/5 flex items-start relative right-0">${profile_img_slider}</span>
                 </span>
 
                 <a href="#" class="text-sm absolute -bottom-10 sm:bottom-0 sm:left-10 hover:underline opacity-90">+ Follow</a>
                 <div class="w-2/5">
                     <span class="w-72 block overflow-hidden">
-                        <span id="brandName" style="transition:1.5s;" class="relative right-0 flex items-center">${title_slider}</span>
+                        <span id="brandName" class="transition-1/5 relative right-0 flex items-center">${title_slider}</span>
                     </span>
                     
                     <span class="sm:w-4/5 mt-6 h-22 block opacity-80 relative text-sm">
